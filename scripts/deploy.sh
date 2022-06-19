d2b37521005afb055e76b05098f5fcb924b42080
kim jin seong <wlsdiqkdrk@naver.com>
2022-06-19 23:09:35 +0900

    ci/cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,5 +33,6 @@ echo "> $JAR_NAME 실행" >> $REPOSITORY/deploy.log
 
 nohup java -jar \
   -Dspring.config.location=/home/ec2-user/app/application.yml \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & >> $REPOSITORY/deploy.log
+  $REPOSITORY/$JAR_NAME 2>&1 &
+
 

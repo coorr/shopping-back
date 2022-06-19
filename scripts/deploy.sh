cf8cc4dc7cea782123c521633b83d39c4bd3d988
kim jin seong <wlsdiqkdrk@naver.com>
2022-06-19 16:38:20 +0900

    ci/cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,6 +31,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \-Dspring.config.location=/home/ec2-user/app/application.yml \$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
+nohup java -jar \
+  -Dspring.config.location=/home/ec2-user/app/application.yml \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 

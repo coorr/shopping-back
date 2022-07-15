1c17653b7ba18d6db508debba85ab38d89012f39
kimjinseong <wlsdiqkdrk@naver.com>
2022-07-15 09:15:53 +0900

    git account change

--- src/main/java/shopping/coor/item/presentation/http/ItemQueryController.java
+++ src/main/java/shopping/coor/item/presentation/http/ItemQueryController.java
@@ -2,9 +2,11 @@ package shopping.coor.item.presentation.http;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shopping.coor.item.application.query.ItemQuery;
+import shopping.coor.item.presentation.http.request.ItemsGetReqDto;
 
 @RestController
 @RequestMapping(value = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -13,10 +15,10 @@ public class ItemQueryController {
 
     private final ItemQuery query;
 
-//    @GetMapping("/items")
-//    public ResponseEntity<List<ItemsGetResDto>> getItems(ItemsGetReqDto itemsGetReqDto) {
+    @GetMapping("/items")
+    public void getItems(ItemsGetReqDto itemsGetReqDto) {
 //        query.
 //        return ResponseEntity.ok().body(getItemCommand.getItemsCommand)
-//    }
+    }
 
 }
